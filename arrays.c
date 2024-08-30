65dd30178d0a6d3d2347e70d2c6c17190c3432fd
Daniel Wang <daniel.wang@try.no>
2024-08-30 10:25:41 +0200

    better code organization

--- arrays.c
+++ arrays.c
@@ -2,6 +2,12 @@
 
 #define N 10
 
+void zero_array(int[], int);
+void read_into_array(int[], int);
+int sum_array(int[], int);
+void print_out_array(int[], int);
+void max_min(int[], int, int*, int*);
+
 void zero_array(int arr[], int size) {
     for(int i=0; i<size; i++) {
         arr[i] = 0;
@@ -31,15 +37,27 @@ void print_out_array(int arr[], int size) {
     printf("\n");
 }
 
+// pointer fun
+void max_min(int a[], int n, int *max, int *min) {
+    int i;
+
+    *max = *min = a[0];
+    for(i=1; i<n; i++) {
+        if(a[i] > *max)
+            *max = a[i];
+        else if(a[i] < *min)
+            *min = a[i];
+    }
+}
+
 int main() {
     int arr[N];
-
-    zero_array(arr, N);
-    print_out_array(arr, N);
-
     read_into_array(arr, N);
-    int sum = sum_array(arr, N);
-    printf("%d\n", sum);
+
+    int big, small;
+    max_min(arr, N, &big, &small);
+    printf("Largest: %d\n", big);
+    printf("Smallest: %d\n", small);
 
     return 0;
 }
